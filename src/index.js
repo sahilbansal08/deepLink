ae70dcb240b209374d5277d1fbd841adf29bbe93
sahilbansal08 <sahil.bansal@unthinkable.co>
2023-04-14 18:48:57 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-
-const express = require('express');
-const serverless=require('serverless-http')
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const router=express.Router()
+const router = express.Router();
 
-
-app.use('/',express.static('.well-known'));
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use("/", express.static(".well-known"));
+router.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/.netlify/functions/api',router)
+app.use("/.netlify/functions/api", router);
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
 
-module.exports.handler=serverless(app)
+module.exports.handler = serverless(app);
