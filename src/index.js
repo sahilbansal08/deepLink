4895c07b8f48d7caaab31fd9fc55645f6adec7e2
sahilbansal08 <sahil.bansal@unthinkable.co>
2023-04-14 17:15:36 +0530

    s

--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-// const express = require("express");
-// const mongoose=require('mongoose')
-// const app = express();
-// app.use(express.json())
-// app.use(express.urlencoded({extended:true}))
-// mongoose.connect('mongodb+srv://sahilbansal:jsU1WOEHNhf0b3op@cluster0.mhmg0hh.mongodb.net/RestApi').then(()=>{
-//       console.log("connected sucessfully")
-// })
-// const products = require("./products.route");
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
-// app.use("/", products);
-
-// app.use((req, res) => {
-//   res.status(404);
-//   res.send({
-//     error: "not found",
-//   });
-//   res.end();
-// });
-
-// app.listen(3000, () => {
-//   console.log("server started on port 3000");
-// });
 const express = require('express');
 const serverless=require('serverless-http')
 const app = express();
@@ -37,6 +10,7 @@ router.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use('/.netlify/functions/api',router)
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
