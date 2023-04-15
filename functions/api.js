7c7cd9bbeb193a2f663ca66ff4972fb2485e4d30
sahilbansal08 <sahil.bansal@unthinkable.co>
2023-04-15 11:14:49 +0530

    s

--- functions/api.js
+++ functions/api.js
@@ -11,22 +11,18 @@ router.get('/', (req, res) => {
   res.send('App is running..');
 });
 
-//Create new record
 router.post('/add', (req, res) => {
   res.send('New record added.');
 });
 
-//delete existing record
 router.delete('/', (req, res) => {
   res.send('Deleted existing record');
 });
 
-//updating existing record
 router.put('/', (req, res) => {
   res.send('Updating existing record');
 });
 
-//showing demo records
 router.get('/demo', (req, res) => {
   res.json([
     {
